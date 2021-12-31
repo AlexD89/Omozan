38dac5dfb2313924aca94cbbba3922065c7d9d46
Alex D <alexander.dziuba@gmail.com>
2021-12-30 19:30:25 -0500

    Working session utils

--- frontend/omozan.jsx
+++ frontend/omozan.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//Testing
+import { login, logout, signup } from "./util/session_api_utils";
+//testing
 
 document.addEventListener("DOMContentLoaded",()=>{
-    ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"))
+
+    //Testing Area
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+    //Testing Area
+
+    ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
 })
