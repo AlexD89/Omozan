61b3d6f680f75d9250940bcf0dcce58b50339772
Alex D <alexander.dziuba@gmail.com>
2022-01-02 21:39:24 -0500

    Working on products views

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,18 @@
+class Api::ProductsController < ApplicationController
+
+    def index
+        @products = Product.all
+        if @products
+            render :index
+        else
+            render json: ["No products"], status: 404
+    end
+
+    def show
+        @product = Product.find_by(id: params[:id])
+        if @product
+            render :show
+        else
+            render json: ["Product is not found"], status: 404
+    end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+
+    validates :title, :description, :category, :price, presence: true
+    
 end
