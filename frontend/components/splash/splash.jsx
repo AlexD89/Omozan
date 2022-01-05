6d214610ea462456b4bf4673ed9f166fa7c57844
Alex D <alexander.dziuba@gmail.com>
2022-01-05 00:25:43 -0500

    Connected splah links to products

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -21,56 +21,56 @@ class Splash extends React.Component {
                         <div className="img-box">
                             <img src={img1} alt="" />
                         </div>
-                        <Link to="/">Shop Echo glow</Link>
+                        <Link to="/products/7">Shop Echo glow</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Add voice control to any outlet</h3>
                         <div className="img-box">
                             <img src={img2} alt="" />
                         </div>
-                        <Link to="/">Shop Amazon smart plug</Link>
+                        <Link to="/products/8">Shop Amazon smart plug</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Macbook Air M1 2020</h3>
                         <div className="img-box">
                             <img src={img3} alt="" />
                         </div>
-                        <Link to="/">Shop Apple Macbook</Link>
+                        <Link to="/products/1">Shop Apple Macbook</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Atomic Habits by James CLear</h3>
                         <div className="img-box">
                             <img src={img4} alt="" />
                         </div>
-                        <Link to="/">Shop Amazon Books</Link>
+                        <Link to="/products/2">Shop Amazon Books</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Spider-Man: No Way Home</h3>
                         <div className="img-box">
                             <img src={img5} alt="" />
                         </div>
-                        <Link to="/">Shop Spider-Man movie</Link>
+                        <Link to="/products/5">Shop Spider-Man movie</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>New Bose QuietComfort 45</h3>
                         <div className="img-box">
                             <img src={img6} alt="" />
                         </div>
-                        <Link to="/">Shop Bose Headphones</Link>
+                        <Link to="/products/3">Shop Bose Headphones</Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Brushless motor of cordless drill combo kit</h3>
                         <div className="img-box">
                             <img src={img7} alt="" />
                         </div>
-                        <Link to="/">Shop DEWALT tool kit </Link>
+                        <Link to="/products/4">Shop DEWALT tool kit </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Adjustable dumbels for home workout</h3>
                         <div className="img-box">
                             <img src={img8} alt="" />
                         </div>
-                        <Link to="/">Shop Bowflex Dumbbell</Link>
+                        <Link to="/products/6">Shop Bowflex Dumbbell</Link>
                     </div>
                 </ul>
             </div>
