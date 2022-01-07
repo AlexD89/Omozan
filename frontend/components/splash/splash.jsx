05384aef7f8f6ca27b6419e30bba493958b3be1f
Alex D <alexander.dziuba@gmail.com>
2022-01-07 16:57:28 -0500

    Final heroku push for today

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,65 +7,83 @@ class Splash extends React.Component {
     }
 
     render(){
+
+        const linkArr = Object.keys(this.props.products)
         return(
             <div className="splah">
                 <img src={window.splashLogoURL} className="splash-background" />
                 <ul className="splash-tabs">
                     <div className="splash-tab">
                         <h3>Multicolor smart lamp for kids</h3>
-                        <div className="img-box">
-                            <img src={window.img1} alt="" />
-                        </div>
-                        <Link to="/products/7">Shop Echo glow</Link>
+                        <Link to={`/products/${linkArr[6]}`}>
+                            <div className="img-box">
+                                <img src={window.img1} alt="" />
+                            </div>
+                            Shop Echo glow
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Add voice control to any outlet</h3>
-                        <div className="img-box">
-                            <img src={window.img2} alt="" />
-                        </div>
-                        <Link to="/products/8">Shop Amazon smart plug</Link>
+                        <Link to={`/products/${linkArr[7]}`}>
+                            <div className="img-box">
+                                <img src={window.img2} alt="" />
+                            </div>
+                            Shop Amazon smart plug
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Macbook Air M1 2020</h3>
-                        <div className="img-box">
-                            <img src={window.img3} alt="" />
-                        </div>
-                        <Link to="/products/1">Shop Apple Macbook</Link>
+                        <Link to={`/products/${linkArr[0]}`}>
+                            <div className="img-box">
+                                <img src={window.img3} alt="" />
+                            </div>
+                            Shop Apple Macbook
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Atomic Habits by James Clear</h3>
-                        <div className="img-box">
-                            <img src={window.img4} alt="" />
-                        </div>
-                        <Link to="/products/2">Shop Amazon Books</Link>
+                        <Link to={`/products/${linkArr[1]}`}>
+                            <div className="img-box">
+                                <img src={window.img4} alt="" />
+                            </div>
+                            Shop Amazon Books
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Spider-Man: No Way Home</h3>
-                        <div className="img-box">
-                            <img src={window.img5} alt="" />
-                        </div>
-                        <Link to="/products/5">Shop Spider-Man movie</Link>
+                        <Link to={`/products/${linkArr[4]}`}>
+                            <div className="img-box">
+                                <img src={window.img5} alt="" />
+                            </div>
+                            Shop Spider-Man movie
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>New Bose QuietComfort 45</h3>
-                        <div className="img-box">
-                            <img src={window.img6} alt="" />
-                        </div>
-                        <Link to="/products/3">Shop Bose Headphones</Link>
+                        <Link to={`/products/${linkArr[2]}`}>
+                            <div className="img-box">
+                                <img src={window.img6} alt="" />
+                            </div>
+                            Shop Bose Headphones
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Brushless motor of cordless drill combo kit</h3>
-                        <div className="img-box">
-                            <img src={window.img7} alt="" />
-                        </div>
-                        <Link to="/products/4">Shop DEWALT tool kit </Link>
+                        <Link to={`/products/${linkArr[3]}`}>
+                            <div className="img-box">
+                                <img src={window.img7} alt="" />
+                            </div>
+                            Shop DEWALT tool kit 
+                        </Link>
                     </div>
                     <div className="splash-tab">
                         <h3>Adjustable dumbels for home workout</h3>
-                        <div className="img-box">
-                            <img src={window.img8} alt="" />
-                        </div>
-                        <Link to="/products/6">Shop Bowflex Dumbbell</Link>
+                        <Link to={`/products/${linkArr[5]}`}>
+                            <div className="img-box">
+                                <img src={window.img8} alt="" />
+                            </div>
+                            Shop Bowflex Dumbbell
+                        </Link>
                     </div>
                 </ul>
             </div>
