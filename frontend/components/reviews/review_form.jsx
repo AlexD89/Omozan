b15a88991cc02517da9dba435b36fcb573d42ca9
Alex D <alexander.dziuba@gmail.com>
2022-01-08 20:05:26 -0500

    Some styling for review form

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -9,12 +9,28 @@ class ReviewForm extends React.Component {
         const { product } = this.props;
         if (!product) return null;
         return(
-            <div>
-                <h1>Create Review</h1>
-                <img src={product.imageURL}/>
-                <h2>Overall rating</h2>
-                <h2>Add a headline</h2>
-                <h2>Add a written review</h2>
+            <div className="review-page">
+                <div className="review-box">
+                    <h1>Create Review</h1>
+                    <div className="review-form-product">
+                        <div className="review-form-img">
+                            <img src={product.imageURL}/>
+                        </div>
+                        {product.title}
+                    </div>
+                    <div className="review-box-rating">
+                        <h2>Overall rating</h2>
+                        <input type="radio" value={1}/>
+                        <input type="radio" value={2}/>
+                        <input type="radio" value={3}/>
+                        <input type="radio" value={4}/>
+                        <input type="radio" value={5}/>
+                    </div>
+                    <h2>Add a headline</h2>
+                    <input type="text" name="" id="" />
+                    <h2>Add a written review</h2>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
             </div>
         )
     }
