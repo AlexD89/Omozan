0b9fd53bfc3fd604a08c60d9466324bacdbdbe3f
Alex D <alexander.dziuba@gmail.com>
2022-01-09 12:04:16 -0500

    Added avg score to Products

--- app/views/api/products/show.json.jbuilder
+++ app/views/api/products/show.json.jbuilder
@@ -1,5 +1,3 @@
 json.set! @product.id do
     json.partial! "product", product: @product
-    # json.imageURL url_for(@product.image)
-    json.imageURL url_for("https://m.media-amazon.com/images/I/41R0JzQ1JLL.jpg")
 end
--- frontend/components/reviews/reviews_item.jsx
+++ frontend/components/reviews/reviews_item.jsx
@@ -1,6 +1,36 @@
 import React from "react";
 
 const ReviewItem = (props) => {
+
+    const avgScore = score => {
+        switch (score) {
+            case (score > 0 && score < 0.5):
+                return "avg-1";
+            case (score >= 0.5 && score < 1):
+                return "avg-2";
+            case (score >= 1 && score < 1.5):
+                return "avg-3";
+            case (score >= 1.5 && score < 2):
+                return "avg-4";
+            case (score >= 2 && score < 2.5):
+                return "avg-5";
+            case (score >= 2.5 && score < 3):
+                return "avg-6";
+            case (score >= 3 && score < 3.5):
+                return "avg-7";
+            case (score >= 3.5 && score < 4):
+                return "avg-8";
+            case (score >= 4 && score < 4.3):
+                return "avg-9";
+            case (score >= 4.3 && score < 4.8):
+                return "avg-10";
+            case (score >= 4.8 && score <= 5):
+                return "avg-11";
+            default:
+                return "avg-0";
+        }
+    }
+
     const {review} = props;
     return(
         <div className="single-review">
@@ -10,7 +40,6 @@ const ReviewItem = (props) => {
             </div>
             <div className="review-title">
                 <div className={`temp num-${review.score}`}></div>    
-                {/* <img src={window.starsURL} alt="score" /> */}
                 {review.title}
             </div>
             <p>Verified Purcahse</p>
