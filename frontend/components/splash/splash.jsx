ab70d9a8b879ae37230b794ca5197081d25d74ad
Alex D <alexander.dziuba@gmail.com>
2022-01-13 20:24:16 -0500

    Fixed splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -10,100 +10,114 @@ class Splash extends React.Component {
         this.props.clearProducts()
     }
 
-    render(){
+    pickSplash = () => {
+        const arr = []
+        const products = Object.values(this.props.products)
+        for(let i=0; i<56; i+=7){
+            arr.push(products[i])
+        }
+        return arr;
+    }
 
-        // const linkArr = Object.keys(this.props.products)
+
+    render(){
+        if (Object.values(this.props.products).length < 8) return null;
+        const productArr = this.pickSplash();
+        console.log(productArr[0])
+        const linkArr = Object.keys(this.props.products)
+        const {products} = this.props
         return(
             <div className="splah">
                 <img src={window.splashLogoURL} className="splash-background" />
                 <ul className="splash-tabs">
                     <div className="splash-tab">
-                        <h3>Multicolor smart lamp for kids</h3>
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            <div className="img-box">
-                                <img src={window.img1} alt="" />
-                            </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Echo glow
-                        {/* </Link> */}
-                    </div>
-                    <div className="splash-tab">
-                        <h3>Add voice control to any outlet</h3>
-                        {/* <Link to={`/products/${linkArr[7]}`}> */}
-                            <div className="img-box">
-                                <img src={window.img2} alt="" />
-                            </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Amazon smart plug
-                        {/* </Link> */}
-                    </div>
-                    <div className="splash-tab">
                         <h3>Macbook Air M1 2020</h3>
-                        {/* <Link to={`/products/${linkArr[0]}`}> */}
+                        {console.log(productArr)}
+                        <Link to={`/products/${productArr[0].id}`}>
                             <div className="img-box">
-                                <img src={window.img3} alt="" />
+                            <img src={productArr[0].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Apple Macbook
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[0].id}`}>
+                            Shop Apple MacBook
+                        </Link>
                     </div>
                     <div className="splash-tab">
-                        <h3>Atomic Habits by James Clear</h3>
-                        {/* <Link to={`/products/${linkArr[1]}`}> */}
+                        <h3>Vibe All-in-one Computer Real-time Interactive Whiteboard</h3>
+                        <Link to={`/products/${productArr[1].id}`}>
                             <div className="img-box">
-                                <img src={window.img4} alt="" />
+                                <img src={productArr[1].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Amazon Books
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[1].id}`}>
+                            Shop Vibe
+                        </Link>
                     </div>
                     <div className="splash-tab">
-                        <h3>Spider-Man: No Way Home</h3>
-                        {/* <Link to={`/products/${linkArr[4]}`}> */}
+                        <h3>WLIVE 2 Drawer Dresser</h3>
+                        <Link to={`/products/${productArr[2].id}`}>
                             <div className="img-box">
-                                <img src={window.img5} alt="" />
+                                <img src={productArr[2].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Spider-Man movie
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[2].id}`}>
+                            Shop WLIVE furniture
+                        </Link>
                     </div>
                     <div className="splash-tab">
-                        <h3>New Bose QuietComfort 45</h3>
-                        {/* <Link to={`/products/${linkArr[2]}`}> */}
+                        <h3>Meow Mix Original Choice </h3>
+                        <Link to={`/products/${productArr[3].id}`}>
                             <div className="img-box">
-                                <img src={window.img6} alt="" />
+                                <img src={productArr[3].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Bose Headphones
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[3].id}`}>
+                            Shop Cats food
+                        </Link>
                     </div>
                     <div className="splash-tab">
-                        <h3>Brushless motor of cordless drill combo kit</h3>
-                        {/* <Link to={`/products/${linkArr[3]}`}> */}
+                        <h3>Moto G Power</h3>
+                        <Link to={`/products/${productArr[4].id}`}>
                             <div className="img-box">
-                                <img src={window.img7} alt="" />
+                                <img src={productArr[4].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop DEWALT tool kit 
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[4].id}`}>
+                            Shop Mototrola cellphone
+                        </Link>
                     </div>
                     <div className="splash-tab">
-                        <h3>Adjustable dumbels for home workout</h3>
-                        {/* <Link to={`/products/${linkArr[5]}`}> */}
+                        <h3>iMucci Pink</h3>
+                        <Link to={`/products/${productArr[5].id}`}>
                             <div className="img-box">
-                                <img src={window.img8} alt="" />
+                                <img src={productArr[5].imageURL} alt="" />
                             </div>
-                        {/* </Link> */}
-                        {/* <Link to={`/products/${linkArr[6]}`}> */}
-                            Shop Bowflex Dumbbell
-                        {/* </Link> */}
+                        </Link>
+                        <Link to={`/products/${productArr[5].id}`}>
+                            Shop Bathroom Accessories Set
+                        </Link>
+                    </div>
+                    <div className="splash-tab">
+                        <h3>Ball Regular Mouth</h3>
+                        <Link to={`/products/${productArr[6].id}`}>
+                            <div className="img-box">
+                                <img src={productArr[6].imageURL} alt="" />
+                            </div>
+                        </Link>
+                        <Link to={`/products/${productArr[6].id}`}>
+                            Mason Jar with Lids  
+                        </Link>
+                    </div>
+                    <div className="splash-tab">
+                        <h3>New Bose SoundLink Flex</h3>
+                        <Link to={`/products/${productArr[7].id}`}>
+                            <div className="img-box">
+                                <img src={productArr[7].imageURL} alt="" />
+                            </div>
+                        </Link>
+                        <Link to={`/products/${productArr[7].id}`}>
+                            Shop Bose Speakers
+                        </Link>
                     </div>
                 </ul>
             </div>
