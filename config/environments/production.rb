c4c57dea4b1ff23c9ebd54560712e0950473f710
Alex D <alexander.dziuba@gmail.com>
2023-02-24 01:04:58 -0500

    setup aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   
-  # config.active_storage.service = :amazon_prod
+  config.active_storage.service = :amazon_prod
 end
